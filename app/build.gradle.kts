8445266813e4c3ff1c89975d6b82cee46f22a833
VividHydra28 <thatomahao28@gmail.com>
2023-11-14 18:12:41 +0200

    journal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //implementation ("com.azure.storage.blob:azure-storage-blob:12.14.0")
 
     implementation ("com.google.android.gms:play-services-maps:17.0.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
@@ -67,6 +68,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/loginpage/JournalFragment.kt
+++ app/src/main/java/com/example/loginpage/JournalFragment.kt
@@ -1,3 +1,4 @@
+
 package com.example.loginpage
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +7,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.FirebaseDatabase
+
+//import com.azure.storage.blob.BlobServiceClientBuilder
+//import com.azure.storage.blob.specialized.BlockBlobClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class JournalFragment : Fragment() {
 
@@ -15,7 +24,15 @@ class JournalFragment : Fragment() {
     private lateinit var editBirdLongitude: EditText
     private lateinit var btnAddBird: Button
 
+    val database = FirebaseDatabase.getInstance()
+    val myDB = database.getReference("message")
+
+   // myDB.setValue("Database connected")
+
     private val birdList = mutableListOf<Bird>()
+
+   // val blobContainerName = "opensource7312" // Set your Azure Blob Storage container name
+    //val connectionString = "DefaultEndpointsProtocol=https;AccountName=opensource7312;AccountKey=r/+IftxJviDVRnMUT3IWEUlQANJE3GWKX/WRC4clYllL63eeBsfD9rJP+RIud+NyhhO9wPzK2Lw++AStWXbttw==;EndpointSuffix=core.windows.net" // Blob storage connection string
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,6 +57,15 @@ class JournalFragment : Fragment() {
                 val bird = Bird(name, description, R.drawable.ic_bird, latitude, longitude)
                 birdList.add(bird)
 
+
+                // Get a reference to the "birds" node in your Firebase database
+                val birdsRef = database.getReference("birds")
+
+                // Push the bird data to the database
+                val newBirdRef = birdsRef.push()
+                newBirdRef.setValue(bird)
+
+
                 // Optionally, you can clear the input fields after adding the bird
                 clearInputFields()
 
@@ -55,6 +81,8 @@ class JournalFragment : Fragment() {
         return rootView
     }
 
+
+
     private fun clearInputFields() {
         editBirdName.text.clear()
         editBirdDescription.text.clear()
@@ -69,3 +97,4 @@ data class Bird(
     val latitude: Double,
     val longitude: Double
 )
+
